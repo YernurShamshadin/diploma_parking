1bb2e468ddd65b8ab002d8d7cac711af68ef6672
YernurShamshadin <ernurshamshadin@gmail.com>
2024-02-28 23:53:21 +0600

    post crud and swagger installation

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Requests\Post\UpdateRequest;
+use App\Http\Resources\Post\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+
+        return PostResource::collection($posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        $post = Post::create($data);
+
+        return PostResource::make($post);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return PostResource::make(Post::find($id));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRequest $request, string $id)
+    {
+        $data = $request->validated();
+        $post = Post::find($id);
+        $post->update($data);
+
+        return PostResource::make($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+        $post->delete();
+
+        return response()->json([
+           'message' => 'Post deleted successfully',
+        ]);
+    }
+}
